4e9236b2a8125d4a7677c62340895006a5f17b80
njmmatthieu <26273598+njmmatthieu@users.noreply.github.com>
2023-10-25 17:58:51 +0200

    last version for supplementary information

--- Rehman_CF_NCF_other_methods.R
+++ Rehman_CF_NCF_other_methods.R
@@ -113,20 +113,31 @@ plage_heatmap <- pheatmap(plage_output_df,
 # plage_heatmap
 # dev.off()
 
-
-
 # Perform t-tests for each gene
-plage_t_test_p_values <- apply(X = plage_output_df,
-                               MARGIN = 1,
-                               FUN = function(x) {
-                                 t_test_result <- t.test(x[CF_samples], x[non_CF_samples])
-                                 return(t_test_result$p.value)
-                                 })
+plage_t_test <- data.frame(t(apply(X = plage_output_df,
+                                   MARGIN = 1,
+                                   FUN = function(x) {
+                                     t_test_result <- t.test(x[CF_samples], x[non_CF_samples])
+                                     print(class(t_test_result))
+                                     return(c(t_test_result$statistic, t_test_result$p.value))
+                                   })))
+colnames(plage_t_test) <- c("statistic", "p.value")
 
-plage_adjusted_p_values <- p.adjust(plage_t_test_p_values, 
-                              method = "fdr")
+plage_t_test$statistic.abs <- abs(plage_t_test$statistic)
+plage_t_test$adj.p.value <- p.adjust(plage_t_test$p.value, 
+                                     method = "fdr")
 
-length(plage_adjusted_p_values[plage_adjusted_p_values < 0.05])
+plage_t_test <- plage_t_test[order(plage_t_test$statistic.abs, decreasing = T),]
+
+write.table(plage_t_test,
+          file = "/Users/matthieu/ownCloud/Thèse/Systems Biology/project rROMA/rRoma_comp/Rehman_CF_NCF_plage_t_test_results.tsv",
+          sep = "\t",
+          row.names = T,
+          row)
+
+
+
+length(which(plage_t_test$adj.p.value < 0.05))
 length(plage_adjusted_p_values < 0.05)
 
 # GSVA
@@ -160,19 +171,27 @@ gsva_heatmap <- pheatmap(gsva_output_df,
 # dev.off()
 
 # Perform t-tests for each gene
-gsva_t_test_p_values <- apply(X = gsva_output_df,
-                         MARGIN = 1,
-                         FUN = function(x) {
-                           # print(x[CF_samples])
-                           # print(x[non_CF_samples])
-                           t_test_result <- t.test(x[CF_samples], x[non_CF_samples])
-                           return(t_test_result$p.value)
-                         })
+gsva_t_test <- data.frame(t(apply(X = gsva_output_df,
+                                  MARGIN = 1,
+                                  FUN = function(x) {
+                                    t_test_result <- t.test(x[CF_samples], x[non_CF_samples])
+                                    print(class(t_test_result))
+                                    return(c(t_test_result$statistic, t_test_result$p.value))
+                                  })))
+colnames(gsva_t_test) <- c("statistic", "p.value")
 
-gsva_adjusted_p_values <- p.adjust(gsva_t_test_p_values, 
-                              method = "fdr")
+gsva_t_test$statistic.abs <- abs(gsva_t_test$statistic)
+gsva_t_test$adj.p.value <- p.adjust(gsva_t_test$p.value, 
+                                    method = "fdr")
 
-length(gsva_adjusted_p_values[gsva_adjusted_p_values < 0.05])
+gsva_t_test <- gsva_t_test[order(gsva_t_test$statistic.abs, decreasing = T),]
+
+write.table(gsva_t_test[,c("statistic","adj.p.value")],
+            file = "/Users/matthieu/ownCloud/Thèse/Systems Biology/project rROMA/rRoma_comp/Rehman_CF_NCF_gsva_t_test_results.tsv",
+            sep = "\t",
+            row.names = T)
+
+length(which(gsva_t_test$adj.p.value < 0.05))
 table(gsva_adjusted_p_values < 0.05)
 
 pheatmap(gsva_output_df)
@@ -207,19 +226,27 @@ ssgsea_heatmap <- pheatmap(ssgsea_output_df,
 # dev.off()
 
 # Perform t-tests for each gene
-ssgsea_t_test_p_values <- apply(X = ssgsea_output_df,
-                              MARGIN = 1,
-                              FUN = function(x) {
-                                # print(x[CF_samples])
-                                # print(x[non_CF_samples])
-                                t_test_result <- t.test(x[CF_samples], x[non_CF_samples])
-                                return(t_test_result$p.value)
-                              })
+ssgsea_t_test <- data.frame(t(apply(X = ssgsea_output_df,
+                                    MARGIN = 1,
+                                    FUN = function(x) {
+                                      t_test_result <- t.test(x[CF_samples], x[non_CF_samples])
+                                      return(c(t_test_result$statistic, t_test_result$p.value))
+                                    })))
+colnames(ssgsea_t_test) <- c("statistic", "p.value")
 
-ssgsea_adjusted_p_values <- p.adjust(ssgsea_t_test_p_values, 
-                                   method = "fdr")
+ssgsea_t_test$statistic.abs <- abs(ssgsea_t_test$statistic)
+ssgsea_t_test$adj.p.value <- p.adjust(ssgsea_t_test$p.value, 
+                                      method = "fdr")
 
-names(ssgsea_adjusted_p_values[ssgsea_adjusted_p_values < 0.05])
+ssgsea_t_test <- ssgsea_t_test[order(ssgsea_t_test$statistic.abs, decreasing = T),]
+
+write.table(ssgsea_t_test[,c("statistic","adj.p.value")],
+            file = "/Users/matthieu/ownCloud/Thèse/Systems Biology/project rROMA/rRoma_comp/Rehman_CF_NCF_ssgsea_t_test_results.tsv",
+            sep = "\t",
+            row.names = T)
+
+
+length(which(ssgsea_t_test$adj.p.value < 0.05))
 table(ssgsea_adjusted_p_values < 0.05)
 
 pheatmap(ssgsea_output_df)
@@ -238,6 +265,8 @@ colnames(zscore_output_df) <- gsub(pattern = "_",
 colnames(zscore_output_df) <- gsub(pattern = "[.]",
                                    replacement = " ",
                                    x = colnames(zscore_output_df))
+
+
 
 zscore_mycolors <- colorRampPalette(brewer.pal(3, "RdBu"))(50)
 zscore_pal <- colorRampPalette(rev(RColorBrewer::brewer.pal(n = 7, name = "PiYG")))(100)
@@ -258,19 +287,26 @@ zscore_heatmap <- pheatmap(zscore_output_df,
 # dev.off()
 
 # Perform t-tests for each gene
-zscore_t_test_p_values <- apply(X = zscore_output_df,
-                                MARGIN = 1,
-                                FUN = function(x) {
-                                  # print(x[CF_samples])
-                                  # print(x[non_CF_samples])
-                                  t_test_result <- t.test(x[CF_samples], x[non_CF_samples])
-                                  return(t_test_result$p.value)
-                                })
+zscore_t_test <- data.frame(t(apply(X = zscore_output_df,
+                                    MARGIN = 1,
+                                    FUN = function(x) {
+                                      t_test_result <- t.test(x[CF_samples], x[non_CF_samples])
+                                      return(c(t_test_result$statistic, t_test_result$p.value))
+                                    })))
+colnames(zscore_t_test) <- c("statistic", "p.value")
 
-zscore_adjusted_p_values <- p.adjust(zscore_t_test_p_values, 
-                                     method = "fdr")
+zscore_t_test$statistic.abs <- abs(zscore_t_test$statistic)
+zscore_t_test$adj.p.value <- p.adjust(zscore_t_test$p.value, 
+                                      method = "fdr")
 
-length(zscore_adjusted_p_values[zscore_adjusted_p_values < 0.05])
+zscore_t_test <- zscore_t_test[order(zscore_t_test$statistic.abs, decreasing = T),]
+
+write.table(zscore_t_test[,c("statistic","adj.p.value")],
+            file = "/Users/matthieu/ownCloud/Thèse/Systems Biology/project rROMA/rRoma_comp/Rehman_CF_NCF_zscore_t_test_results.tsv",
+            sep = "\t",
+            row.names = T)
+
+length(which(zscore_t_test$adj.p.value < 0.05))
 table(zscore_adjusted_p_values < 0.05)
 
 pheatmap(zscore_output_df)
@@ -301,20 +337,27 @@ rRoma_heatmap <- pheatmap(rROMA_output_df,
                            breaks = myBreaks,
                            border_color = "NA")
 
-# Perform t-tests for each gene
-rROMA_t_test_p_values <- apply(X = rROMA_output_df,
-                                MARGIN = 1,
-                                FUN = function(x) {
-                                  # print(x[CF_samples])
-                                  # print(x[non_CF_samples])
-                                  t_test_result <- t.test(x[CF_samples], x[non_CF_samples])
-                                  return(t_test_result$p.value)
-                                })
+rROMA_t_test <- data.frame(t(apply(X = rROMA_output_df,
+                                   MARGIN = 1,
+                                   FUN = function(x) {
+                                     t_test_result <- t.test(x[CF_samples], x[non_CF_samples])
+                                     return(c(t_test_result$statistic, t_test_result$p.value))
+                                   })))
+colnames(rROMA_t_test) <- c("statistic", "p.value")
 
-rROMA_adjusted_p_values <- p.adjust(rROMA_t_test_p_values, 
+rROMA_t_test$statistic.abs <- abs(rROMA_t_test$statistic)
+rROMA_t_test$adj.p.value <- p.adjust(rROMA_t_test$p.value, 
                                      method = "fdr")
 
-length(rROMA_adjusted_p_values[rROMA_adjusted_p_values < 0.05])
+rROMA_t_test <- rROMA_t_test[order(rROMA_t_test$statistic.abs, decreasing = T),]
+
+write.table(rROMA_t_test[,c("statistic","adj.p.value")],
+            file = "/Users/matthieu/ownCloud/Thèse/Systems Biology/project rROMA/rRoma_comp/Rehman_CF_NCF_rROMA_t_test_results.tsv",
+            sep = "\t",
+            row.names = T)
+
+length(which(rROMA_t_test$adj.p.value<0.05))
+which(rownames(rROMA_t_test)=="TNFA SIGNALING VIA NFKB")
 
 # png(filename = "./Rehman_CF_NCF_rRoma_heatmap.png", 
 #     width = 1800, 
--- Rehman_TNF_IL17_gsva.R
+++ Rehman_TNF_IL17_gsva.R
@@ -1,6 +1,8 @@
 library(GSVA)
 library(ggfortify)
 library(ggplot2)
+library(pheatmap)
+library(RColorBrewer)
 library(tidyr)
 library(tidyverse)
 
@@ -86,24 +88,38 @@ plage_heatmap <- pheatmap(plage_output_df,
                           breaks = myBreaks,
                           border_color = "NA")
 
-png(filename = "./Rehman_IL17_TNFa_plage_heatmap.png", 
-    width = 1800, 
-    height = 1500, 
-    res = 150)
-plage_heatmap
-dev.off()
+# png(filename = "./Rehman_IL17_TNFa_plage_heatmap.png", 
+#     width = 1800, 
+#     height = 1500, 
+#     res = 150)
+# plage_heatmap
+# dev.off()
 
 
 # Perform t-tests for each gene
-plage_t_test_p_values <- apply(X = plage_output_df,
+plage_t_test <- data.frame(t(apply(X = plage_output_df,
                                MARGIN = 1,
                                FUN = function(x) {
                                  t_test_result <- t.test(x[control_samples], x[inf_samples])
-                                 return(t_test_result$p.value)
-                               })
+                                 print(class(t_test_result))
+                                 return(c(t_test_result$statistic, t_test_result$p.value))
+                               })))
+colnames(plage_t_test) <- c("statistic", "p.value")
 
-plage_adjusted_p_values <- p.adjust(plage_t_test_p_values, 
+plage_t_test$statistic.abs <- abs(plage_t_test$statistic)
+plage_t_test$adj.p.value <- p.adjust(plage_t_test$p.value, 
                                     method = "fdr")
+
+plage_t_test <- plage_t_test[order(plage_t_test$statistic.abs, decreasing = T),]
+
+write.table(plage_t_test[,c("statistic","adj.p.value")],
+            file = "/Users/matthieu/ownCloud/Thèse/Systems Biology/project rROMA/rRoma_comp/Rehman_TNF_IL17_plage_t_test_results.tsv",
+            sep = "\t",
+            row.names = T)
+
+
+length(which(plage_t_test$adj.p.value<0.05))
+
 
 names(plage_adjusted_p_values[plage_adjusted_p_values < 0.05])
 length(plage_adjusted_p_values < 0.05)
@@ -123,37 +139,50 @@ gsva_es <- gsva(Rehman_exp_RNA_matrix_norm,
 gsva_output_df <- data.frame(gsva_es)
 colnames(gsva_output_df) <- Rehman_exp_coldata$Simple_id
 
-gsva_heatmap <- pheatmap(gsva_output_df,
-                          annotation_col = condition,
-                          annotation_colors = condition_colors,
-                          color = pal,
-                          breaks = myBreaks,
-                          border_color = "NA")
+# gsva_heatmap <- pheatmap(gsva_output_df,
+#                           annotation_col = condition,
+#                           annotation_colors = condition_colors,
+#                           color = pal,
+#                           breaks = myBreaks,
+#                           border_color = "NA")
 
-png(filename = "./Rehman_IL17_TNFa_gsva_heatmap.png", 
-    width = 1800, 
-    height = 1500, 
-    res = 150)
-gsva_heatmap
-dev.off()
+# png(filename = "./Rehman_IL17_TNFa_gsva_heatmap.png", 
+#     width = 1800, 
+#     height = 1500, 
+#     res = 150)
+# gsva_heatmap
+# dev.off()
 
 # Perform t-tests for each gene
-gsva_t_test_p_values <- apply(X = gsva_output_df,
-                              MARGIN = 1,
-                              FUN = function(x) {
-                                t_test_result <- t.test(x[control_samples], x[inf_samples])
-                                return(t_test_result$p.value)
-                              })
+gsva_t_test <- data.frame(t(apply(X = gsva_output_df,
+                                   MARGIN = 1,
+                                   FUN = function(x) {
+                                     t_test_result <- t.test(x[control_samples], x[inf_samples])
+                                     print(class(t_test_result))
+                                     return(c(t_test_result$statistic, t_test_result$p.value))
+                                   })))
+colnames(gsva_t_test) <- c("statistic", "p.value")
 
-gsva_adjusted_p_values <- p.adjust(gsva_t_test_p_values, 
-                                   method = "fdr")
+gsva_t_test$statistic.abs <- abs(gsva_t_test$statistic)
+gsva_t_test$adj.p.value <- p.adjust(gsva_t_test$p.value, 
+                                     method = "fdr")
+
+gsva_t_test <- gsva_t_test[order(gsva_t_test$statistic.abs, decreasing = T),]
+
+write.table(gsva_t_test[,c("statistic","adj.p.value")],
+            file = "/Users/matthieu/ownCloud/Thèse/Systems Biology/project rROMA/rRoma_comp/Rehman_TNF_IL17_gsva_t_test_results.tsv",
+            sep = "\t",
+            row.names = T)
+
+length(which(gsva_t_test$adj.p.value<0.05))
+which(rownames(gsva_t_test)=="TNFA SIGNALING VIA NFKB")
 
 names(gsva_adjusted_p_values[gsva_adjusted_p_values < 0.05])
 table(gsva_adjusted_p_values < 0.05)
 
 gsva_adjusted_p_values[order(gsva_adjusted_p_values, decreasing = F)]
 
-pheatmap(gsva_output_df)
+# pheatmap(gsva_output_df)
 
 ## SSGSEA
 
@@ -172,25 +201,35 @@ ssgsea_heatmap <- pheatmap(ssgsea_output_df,
                          breaks = myBreaks,
                          border_color = "NA")
 
-png(filename = "./Rehman_IL17_TNFa_ssgsea_heatmap.png", 
-    width = 1800, 
-    height = 1500, 
-    res = 150)
-ssgsea_heatmap
-dev.off()
+# png(filename = "./Rehman_IL17_TNFa_ssgsea_heatmap.png", 
+#     width = 1800, 
+#     height = 1500, 
+#     res = 150)
+# ssgsea_heatmap
+# dev.off()
 
 # Perform t-tests for each gene
-ssgsea_t_test_p_values <- apply(X = ssgsea_output_df,
-                                MARGIN = 1,
-                                FUN = function(x) {
-                                  # print(x[CF_samples])
-                                  # print(x[non_CF_samples])
-                                  t_test_result <- t.test(x[control_samples], x[inf_samples])
-                                  return(t_test_result$p.value)
-                                })
+ssgsea_t_test <- data.frame(t(apply(X = ssgsea_output_df,
+                                  MARGIN = 1,
+                                  FUN = function(x) {
+                                    t_test_result <- t.test(x[control_samples], x[inf_samples])
+                                    return(c(t_test_result$statistic, t_test_result$p.value))
+                                  })))
+colnames(ssgsea_t_test) <- c("statistic", "p.value")
 
-ssgsea_adjusted_p_values <- p.adjust(ssgsea_t_test_p_values, 
-                                     method = "fdr")
+ssgsea_t_test$statistic.abs <- abs(ssgsea_t_test$statistic)
+ssgsea_t_test$adj.p.value <- p.adjust(ssgsea_t_test$p.value, 
+                                    method = "fdr")
+
+ssgsea_t_test <- ssgsea_t_test[order(ssgsea_t_test$statistic.abs, decreasing = T),]
+
+write.table(ssgsea_t_test[,c("statistic","adj.p.value")],
+            file = "/Users/matthieu/ownCloud/Thèse/Systems Biology/project rROMA/rRoma_comp/Rehman_TNF_IL17_ssgsea_t_test_results.tsv",
+            sep = "\t",
+            row.names = T)
+
+length(which(ssgsea_t_test$adj.p.value<0.05))
+which(rownames(ssgsea_t_test)=="TNFA SIGNALING VIA NFKB")
 
 names(ssgsea_adjusted_p_values[ssgsea_adjusted_p_values < 0.05])
 table(ssgsea_adjusted_p_values < 0.05)
@@ -209,6 +248,28 @@ zscore_es <- gsva(Rehman_exp_RNA_matrix_norm,
 zscore_output_df <- data.frame(zscore_es)
 colnames(zscore_output_df) <- Rehman_exp_coldata$Simple_id
 
+zscore_t_test <- data.frame(t(apply(X = zscore_output_df,
+                                    MARGIN = 1,
+                                    FUN = function(x) {
+                                      t_test_result <- t.test(x[control_samples], x[inf_samples])
+                                      return(c(t_test_result$statistic, t_test_result$p.value))
+                                    })))
+colnames(zscore_t_test) <- c("statistic", "p.value")
+
+zscore_t_test$statistic.abs <- abs(zscore_t_test$statistic)
+zscore_t_test$adj.p.value <- p.adjust(zscore_t_test$p.value, 
+                                      method = "fdr")
+
+zscore_t_test <- zscore_t_test[order(zscore_t_test$statistic.abs, decreasing = T),]
+
+write.table(zscore_t_test[,c("statistic","adj.p.value")],
+            file = "/Users/matthieu/ownCloud/Thèse/Systems Biology/project rROMA/rRoma_comp/Rehman_TNF_IL17_zscore_t_test_results.tsv",
+            sep = "\t",
+            row.names = T)
+
+length(which(zscore_t_test$adj.p.value<0.05))
+which(rownames(zscore_t_test)=="TNFA SIGNALING VIA NFKB")
+
 zscore_pal <- colorRampPalette(rev(RColorBrewer::brewer.pal(n = 7, name = "PiYG")))(100)
 zscore_myBreaks <- seq(min(zscore_output_df), max(zscore_output_df), length.out = 100)
 
@@ -219,12 +280,12 @@ zscore_heatmap <- pheatmap(zscore_output_df,
                            breaks = zscore_myBreaks,
                            border_color = "NA")
 
-png(filename = "./Rehman_IL17_TNFa_zscore_heatmap.png", 
-    width = 1800, 
-    height = 1500, 
-    res = 150)
-zscore_heatmap
-dev.off()
+# png(filename = "./Rehman_IL17_TNFa_zscore_heatmap.png", 
+#     width = 1800, 
+#     height = 1500, 
+#     res = 150)
+# zscore_heatmap
+# dev.off()
 
 # Perform t-tests for each gene
 zscore_t_test_p_values <- apply(X = zscore_output_df,
@@ -240,16 +301,35 @@ zscore_t_test_p_values <- apply(X = zscore_output_df,
 zscore_adjusted_p_values <- p.adjust(zscore_t_test_p_values, 
                                      method = "fdr")
 
+
+
 names(zscore_adjusted_p_values[zscore_adjusted_p_values < 0.05])
 table(zscore_adjusted_p_values < 0.05)
 
 zscore_adjusted_p_values[order(zscore_adjusted_p_values, decreasing = F)]
 
-pheatmap(zscore_output_df)
+# pheatmap(zscore_output_df)
 
 ## rROMA
 
 load(file="./Rehman_IL17_TNFa_rRoma_obj.Rdata")
+
+rROMA_stat_outpout_df <- rRoma.output$ModuleMatrix
+
+rownames(rROMA_stat_outpout_df) <- sapply(rownames(rROMA_stat_outpout_df), function(geneset) {
+  geneset_name <- gsub(pattern = "HALLMARK_", 
+                       replacement = "", 
+                       x = geneset)
+  geneset_name <- gsub(pattern = "_", 
+                       replacement = " ", 
+                       x = geneset_name)
+  return(geneset_name)
+})
+
+write.table(rROMA_stat_outpout_df,
+            file = "/Users/matthieu/ownCloud/Thèse/Systems Biology/project rROMA/rRoma_comp/Rehman_TNF_IL17_rROMA_module_matrix.tsv",
+            sep = "\t",
+            row.names = T)
 
 rROMA_output_df <- rRoma.output$SampleMatrix
 colnames(rROMA_output_df) <- gsub(pattern = "_",
@@ -266,6 +346,24 @@ rownames(rROMA_output_df) <- sapply(rownames(rROMA_output_df), function(geneset)
   return(geneset_name)
 })
 
+rROMA_t_test <- data.frame(t(apply(X = rROMA_output_df,
+                                    MARGIN = 1,
+                                    FUN = function(x) {
+                                      t_test_result <- t.test(x[control_samples], x[inf_samples])
+                                      return(c(t_test_result$statistic, t_test_result$p.value))
+                                    })))
+colnames(rROMA_t_test) <- c("statistic", "p.value")
+
+rROMA_t_test$statistic.abs <- abs(rROMA_t_test$statistic)
+rROMA_t_test$adj.p.value <- p.adjust(rROMA_t_test$p.value, 
+                                      method = "fdr")
+
+rROMA_t_test <- rROMA_t_test[order(rROMA_t_test$statistic.abs, decreasing = T),]
+
+length(which(rROMA_t_test$adj.p.value<0.05))
+which(rownames(rROMA_t_test)=="TNFA SIGNALING VIA NFKB")
+
+
 rRoma_heatmap <- pheatmap(rROMA_output_df,
                           annotation_col = condition,
                           annotation_colors = condition_colors,
@@ -273,10 +371,10 @@ rRoma_heatmap <- pheatmap(rROMA_output_df,
                           breaks = myBreaks,
                           border_color = "NA")
 
-png(filename = "./Rehman_IL17_TNFa_rRoma_heatmap.png", 
-    width = 1800, 
-    height = 1500, 
-    res = 150)
-rRoma_heatmap
-dev.off()
+# png(filename = "./Rehman_IL17_TNFa_rRoma_heatmap.png", 
+#     width = 1800, 
+#     height = 1500, 
+#     res = 150)
+# rRoma_heatmap
+# dev.off()
 
